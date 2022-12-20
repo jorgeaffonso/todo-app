facabf0398b7f49c3d8bd1da70eee1691c22e666
Jorge <jorgemail.affonso@gmail.com>
2022-12-20 17:18:44 -0300

    Implement a Edit Buton

--- gui.py
+++ gui.py
@@ -3,11 +3,15 @@ import PySimpleGUI as sg
 
 label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="Enter todo",key='todo')
-add_buton = sg.Button("Add")
-exit_buton = sg.Button("Exit")
+add_button = sg.Button("Add")
+exit_button = sg.Button("Exit")
+edit_button = sg.Button("Edit")
+
+list_box = sg.Listbox(values=functions.get_todos(), key='todos',
+                      enable_events=True, size=[45,10])
 
 window = sg.Window("My To-Do App",
-                   layout=[[label], [input_box, add_buton], [exit_buton]],
+                   layout=[[label], [input_box, add_button], [list_box, edit_button], [exit_button]],
                    font=("Helvetica", 20))
 
 while True:
@@ -21,6 +25,20 @@ while True:
             new_todo = value['todo'] +"\n"
             todos.append(new_todo)
             functions.write_todos(todos)
+            window['todos'].update(values=todos)
+        case 'Edit':
+            todo_to_edit = value['todos'][0]
+            new_todo = value['todo']
+            todos = functions.get_todos()
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+        case 'todos':
+            window['todo'].update(value=value['todos'][0])
         case sg.WIN_CLOSED:
             break
 window.close()
+
+
+
