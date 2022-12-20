548b1c5072c25eedd79cc033836923a298d2a089
Jorge <jorgemail.affonso@gmail.com>
2022-12-20 15:54:18 -0300

    Implement a Add Buton

--- functions.py
+++ functions.py
@@ -1,19 +1,17 @@
-
-def get_todos(filepath="todos.txt"):
+FILEPATH = "todos.txt"
+def get_todos(filepath=FILEPATH):
     """ Read the text file and return the list of
     to-do items. """
     with open(filepath, 'r') as file_local:
         todos_local = file_local.readlines()
     return todos_local
 
-def write_todos(todos_arg,filepath="todos.txt"):
+def write_todos(todos_arg,filepath=FILEPATH):
     """ White the to-dos items List in the text file."""
     with open(filepath, 'w') as file_local:
         file_local.writelines(todos_arg)
         #return not necessary
 
-
-print(__name__)
 
 if __name__ == "__main__":
     print("Hello")
--- gui.py
+++ gui.py
@@ -2,10 +2,25 @@ import functions
 import PySimpleGUI as sg
 
 label = sg.Text("Type in a to-do")
-input_box = sg.InputText(tooltip="Enter todo")
+input_box = sg.InputText(tooltip="Enter todo",key='todo')
 add_buton = sg.Button("Add")
 exit_buton = sg.Button("Exit")
 
-window = sg.Window("My To-Do App", layout=[[label], [input_box, add_buton], [exit_buton]])
-window.read()  # show the window and wait a action
+window = sg.Window("My To-Do App",
+                   layout=[[label], [input_box, add_buton], [exit_buton]],
+                   font=("Helvetica", 20))
+
+while True:
+    event,value = window.read()  # show the window and wait a action. The returno is a tuple
+    print(event)
+    print(value)
+
+    match event:
+        case 'Add':
+            todos = functions.get_todos()
+            new_todo = value['todo'] +"\n"
+            todos.append(new_todo)
+            functions.write_todos(todos)
+        case sg.WIN_CLOSED:
+            break
 window.close()
