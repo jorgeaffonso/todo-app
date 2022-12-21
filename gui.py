298a3398f2c5412164d49700e33bb5865220881d
Jorge <jorgemail.affonso@gmail.com>
2022-12-21 11:22:45 -0300

    Implement the complete e exit Buttons

--- gui.py
+++ gui.py
@@ -6,12 +6,15 @@ input_box = sg.InputText(tooltip="Enter todo",key='todo')
 add_button = sg.Button("Add")
 exit_button = sg.Button("Exit")
 edit_button = sg.Button("Edit")
+complete_button= sg.Button("Complete")
 
 list_box = sg.Listbox(values=functions.get_todos(), key='todos',
                       enable_events=True, size=[45,10])
 
 window = sg.Window("My To-Do App",
-                   layout=[[label], [input_box, add_button], [list_box, edit_button], [exit_button]],
+                   layout=[[label], [input_box, add_button],
+                           [list_box, edit_button, complete_button],
+                           [exit_button]],
                    font=("Helvetica", 20))
 
 while True:
@@ -26,6 +29,7 @@ while True:
             todos.append(new_todo)
             functions.write_todos(todos)
             window['todos'].update(values=todos)
+            window['todo'].update(value='')
         case 'Edit':
             todo_to_edit = value['todos'][0]
             new_todo = value['todo']
@@ -33,7 +37,16 @@ while True:
             index = todos.index(todo_to_edit)
             todos[index] = new_todo
             functions.write_todos(todos)
+            window['todos'].update(value=todos)
+        case 'Complete':
+            todo_to_complete = value['todos'][0]
+            todos = functions.get_todos()
+            todos.remove(todo_to_complete)
+            functions.write_todos(todos)
             window['todos'].update(values=todos)
+            window['todo'].update(value='')
+        case 'Exit':
+            break
         case 'todos':
             window['todo'].update(value=value['todos'][0])
         case sg.WIN_CLOSED:
